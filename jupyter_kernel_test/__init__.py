5ee0bb1220b7cb25ea937cf8976f0d6167ca0877
Gordon Ball <gordon@chronitis.net>
2016-02-03 15:11:55 +0100

    Add tests for error raising, execute and MIME output

--- jupyter_kernel_test/__init__.py
+++ jupyter_kernel_test/__init__.py
@@ -150,3 +150,50 @@ class KernelTests(TestCase):
         # Validate the mimebundle
         MimeBundle().data = mimebundle
         self.assertIn('text/plain', mimebundle)
+
+    code_generate_error = ""
+
+    def test_error(self):
+        if not self.code_generate_error:
+            raise SkipTest
+
+        self.flush_channels()
+
+        reply, output_msgs = self.execute_helper(self.code_generate_error)
+        self.assertEqual(reply['content']['status'], 'error')
+
+    code_output_samples = []
+
+    def test_value(self):
+        if not self.code_output_samples:
+            raise SkipTest
+
+        for sample in self.code_output_samples:
+            self.flush_channels()
+
+            reply, output_msgs = self.execute_helper(sample['code'])
+
+            self.assertEqual(reply['content']['status'], 'ok')
+
+            self.assertGreaterEqual(len(output_msgs), 1)
+            self.assertEqual(output_msgs[0]['msg_type'], 'execute_result')
+            self.assertIn('text/plain', output_msgs[0]['content']['data'])
+            self.assertEqual(output_msgs[0]['content']['data']['text/plain'],
+                             sample['result'])
+
+    code_mime_samples = []
+
+    def test_mime(self):
+        if not self.code_mime_samples:
+            raise SkipTest
+
+        for sample in self.code_mime_samples:
+            self.flush_channels()
+            reply, output_msgs = self.execute_helper(sample['code'])
+
+            self.assertEqual(reply['content']['status'], 'ok')
+
+            self.assertGreaterEqual(len(output_msgs), 1)
+            for om in output_msgs:
+                if om['msg_type'] == 'display_data':
+                    self.assertIn(sample['mime'], om['content']['data'])
