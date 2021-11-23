344e35136c6ee7dd16db8c77fd93b316f48b2a82
Steven Silvester <steven.silvester@ieee.org>
2021-11-22 20:07:40 -0600

    Revert "Merge pull request #41 from jupyter/new-kernel-mods"
    This reverts commit 41850aa9846fd1eb28200f4fceada7c52c7d892d, reversing
    changes made to 0b78c2835fad5df3be182ddd5013a73a63c4f81e.

--- jupyter_kernel_test/__init__.py
+++ jupyter_kernel_test/__init__.py
@@ -4,11 +4,9 @@
 # Distributed under the terms of the Modified BSD License.
 
 from unittest import TestCase, SkipTest
+from queue import Empty
 
-from jupyter_kernel_mgmt.discovery import KernelFinder
-from jupyter_kernel_mgmt.client import BlockingKernelClient
-from tornado import gen
-from tornado.ioloop import IOLoop
+from jupyter_client.manager import start_new_kernel
 from .msgspec_v5 import validate_message
 
 TIMEOUT = 15
@@ -19,89 +17,71 @@ class KernelTests(TestCase):
     kernel_name = ""
 
     @classmethod
-    def launch_kernel(cls):
-        kf = KernelFinder.from_entrypoints()
-        name = cls.kernel_name
-        if '/' not in name:
-            name = 'spec/' + name
-        return kf.launch(name=name)
-
-    @classmethod
     def setUpClass(cls):
-        # Skip tests if it's an instance of KernelTests
-        # See https://github.com/jupyter/jupyter_kernel_test/issues/7
-        if cls is KernelTests:
-            raise SkipTest
-
-        conn_info, km = cls.launch_kernel()
-        cls.kc = BlockingKernelClient(conn_info, manager=km)
-        cls.kc.wait_for_ready(timeout=TIMEOUT)
-        cls.kc.allow_stdin = False
+        cls.km, cls.kc = start_new_kernel(kernel_name=cls.kernel_name)
 
     @classmethod
     def tearDownClass(cls):
-        cls.kc.shutdown_or_terminate()
-        cls.kc.close()
+        cls.kc.stop_channels()
+        cls.km.shutdown_kernel()
 
-    def _wait_for_reply(self, future, timeout=TIMEOUT):
-        return IOLoop.current().run_sync(lambda: future, timeout=timeout)
+    def flush_channels(self):
+        for channel in (self.kc.shell_channel, self.kc.iopub_channel):
+            while True:
+                try:
+                    msg = channel.get_msg(block=True, timeout=0.1)
+                except Empty:
+                    break
+                else:
+                    validate_message(msg)
 
     language_name = ""
     file_extension = ""
 
     def test_kernel_info(self):
-        fut = self.kc.kernel_info(reply=False)
-        reply = self._wait_for_reply(fut)
-        validate_message(reply, 'kernel_info_reply', fut.jupyter_msg_id)
+        self.flush_channels()
+
+        msg_id = self.kc.kernel_info()
+        reply = self.kc.get_shell_msg(timeout=TIMEOUT)
+        validate_message(reply, 'kernel_info_reply', msg_id)
 
         if self.language_name:
-            self.assertEqual(reply.content['language_info']['name'],
+            self.assertEqual(reply['content']['language_info']['name'],
                              self.language_name)
         if self.file_extension:
-            self.assertEqual(reply.content['language_info']['file_extension'],
+            self.assertEqual(reply['content']['language_info']['file_extension'],
                              self.file_extension)
-            self.assertTrue(reply.content['language_info']['file_extension'].startswith("."))
+            self.assertTrue(reply['content']['language_info']['file_extension'].startswith("."))
 
 
 
     def execute_helper(self, code, timeout=TIMEOUT,
                        silent=False, store_history=True,
                        stop_on_error=True):
+        msg_id = self.kc.execute(code=code, silent=silent,
+                                 store_history=store_history,
+                                 stop_on_error=stop_on_error)
+
+        reply = self.kc.get_shell_msg(timeout=timeout)
+        validate_message(reply, 'execute_reply', msg_id)
+
+        busy_msg = self.kc.iopub_channel.get_msg(timeout=1)
+        validate_message(busy_msg, 'status', msg_id)
+        self.assertEqual(busy_msg['content']['execution_state'], 'busy')
+
         output_msgs = []
-        def on_output(msg, _channel):
+        while True:
+            msg = self.kc.iopub_channel.get_msg(timeout=0.1)
+            validate_message(msg, msg['msg_type'], msg_id)
+            if msg['msg_type'] == 'status':
+                self.assertEqual(msg['content']['execution_state'], 'idle')
+                break
+            elif msg['msg_type'] == 'execute_input':
+                self.assertEqual(msg['content']['code'], code)
+                continue
             output_msgs.append(msg)
-        # Give the client a chance to flush messages already arrived
-        IOLoop.current().run_sync(lambda: gen.sleep(0.1))
-        self.kc.loop_client.add_handler(on_output, 'iopub')
-        try:
-            fut = self.kc.execute(
-                code=code, silent=silent, store_history=store_history,
-                stop_on_error=stop_on_error, interrupt_timeout=timeout,
-                reply=False,
-            )
-            reply = self._wait_for_reply(fut, timeout+5)
-        finally:
-            self.kc.loop_client.add_handler(on_output, 'iopub')
 
-        validate_message(reply, 'execute_reply')
-
-        busy_msg = output_msgs[0]
-        validate_message(busy_msg, 'status', fut.jupyter_msg_id)
-        self.assertEqual(busy_msg.content['execution_state'], 'busy')
-
-        real_output_msgs = []   # Excluding status and execute_input
-
-        for msg in output_msgs[1:]:
-            msg_type = msg.header['msg_type']
-            validate_message(msg, msg_type, fut.jupyter_msg_id)
-            if msg_type == 'status':
-                self.assertEqual(msg.content['execution_state'], 'idle')
-            elif msg_type == 'execute_input':
-                self.assertEqual(msg.content['code'], code)
-            else:
-                real_output_msgs.append(msg)
-
-        return reply, real_output_msgs
+        return reply, output_msgs
 
     code_hello_world = ""
 
@@ -109,14 +89,15 @@ class KernelTests(TestCase):
         if not self.code_hello_world:
             raise SkipTest
 
+        self.flush_channels()
         reply, output_msgs = self.execute_helper(code=self.code_hello_world)
 
-        self.assertEqual(reply.content['status'], 'ok')
+        self.assertEqual(reply['content']['status'], 'ok')
 
         self.assertGreaterEqual(len(output_msgs), 1)
         for msg in output_msgs:
-            if (msg.header['msg_type'] == 'stream') and (msg.content['name'] == 'stdout'):
-                self.assertIn('hello, world', msg.content['text'])
+            if (msg['msg_type'] == 'stream') and (msg['content']['name'] == 'stdout'):
+                self.assertIn('hello, world', msg['content']['text'])
                 break
         else:
             self.assertTrue(False, "Expected one output message of type 'stream' and 'content.name'='stdout'")
@@ -127,14 +108,15 @@ class KernelTests(TestCase):
         if not self.code_stderr:
             raise SkipTest
 
+        self.flush_channels()
         reply, output_msgs = self.execute_helper(code=self.code_stderr)
 
-        self.assertEqual(reply.content['status'], 'ok')
+        self.assertEqual(reply['content']['status'], 'ok')
 
         self.assertGreaterEqual(len(output_msgs), 1)
 
         for msg in output_msgs:
-            if (msg.header['msg_type'] == 'stream') and (msg.content['name'] == 'stderr'):
+            if (msg['msg_type'] == 'stream') and (msg['content']['name'] == 'stderr'):
                 break
         else:
             self.assertTrue(False, "Expected one output message of type 'stream' and 'content.name'='stderr'")
@@ -147,11 +129,11 @@ class KernelTests(TestCase):
 
         for sample in self.completion_samples:
             with self.subTest(text=sample['text']):
-                fut = self.kc.complete(sample['text'], reply=False)
-                reply = self._wait_for_reply(fut)
-                validate_message(reply, 'complete_reply', fut.jupyter_msg_id)
+                msg_id = self.kc.complete(sample['text'])
+                reply = self.kc.get_shell_msg()
+                validate_message(reply, 'complete_reply', msg_id)
                 if 'matches' in sample:
-                    self.assertEqual(set(reply.content['matches']),
+                    self.assertEqual(set(reply['content']['matches']),
                                      set(sample['matches']))
 
     complete_code_samples = []
@@ -159,19 +141,21 @@ class KernelTests(TestCase):
     invalid_code_samples = []
 
     def check_is_complete(self, sample, status):
-        fut = self.kc.is_complete(sample, reply=False)
-        reply = self._wait_for_reply(fut)
-        validate_message(reply, 'is_complete_reply', fut.jupyter_msg_id)
-        if reply.content['status'] != status:
+        msg_id = self.kc.is_complete(sample)
+        reply = self.kc.get_shell_msg()
+        validate_message(reply, 'is_complete_reply', msg_id)
+        if reply['content']['status'] != status:
             msg = "For code sample\n  {!r}\nExpected {!r}, got {!r}."
             raise AssertionError(msg.format(sample, status,
-                                            reply.content['status']))
+                                            reply['content']['status']))
 
     def test_is_complete(self):
         if not (self.complete_code_samples
                 or self.incomplete_code_samples
                 or self.invalid_code_samples):
             raise SkipTest
+
+        self.flush_channels()
 
         with self.subTest(status="complete"):
             for sample in self.complete_code_samples:
@@ -191,9 +175,11 @@ class KernelTests(TestCase):
         if not self.code_page_something:
             raise SkipTest
 
+        self.flush_channels()
+
         reply, output_msgs = self.execute_helper(self.code_page_something)
-        self.assertEqual(reply.content['status'],  'ok')
-        payloads = reply.content['payload']
+        self.assertEqual(reply['content']['status'],  'ok')
+        payloads = reply['content']['payload']
         self.assertEqual(len(payloads), 1)
         self.assertEqual(payloads[0]['source'], 'page')
         mimebundle = payloads[0]['data']
@@ -205,10 +191,12 @@ class KernelTests(TestCase):
         if not self.code_generate_error:
             raise SkipTest
 
+        self.flush_channels()
+
         reply, output_msgs = self.execute_helper(self.code_generate_error)
-        self.assertEqual(reply.content['status'], 'error')
+        self.assertEqual(reply['content']['status'], 'error')
         self.assertEqual(len(output_msgs), 1)
-        self.assertEqual(output_msgs[0].header['msg_type'], 'error')
+        self.assertEqual(output_msgs[0]['msg_type'], 'error')
 
     code_execute_result = []
 
@@ -218,14 +206,16 @@ class KernelTests(TestCase):
 
         for sample in self.code_execute_result:
             with self.subTest(code=sample['code']):
+                self.flush_channels()
+
                 reply, output_msgs = self.execute_helper(sample['code'])
 
-                self.assertEqual(reply.content['status'], 'ok')
+                self.assertEqual(reply['content']['status'], 'ok')
 
                 self.assertGreaterEqual(len(output_msgs), 1)
-                self.assertEqual(output_msgs[0].header['msg_type'], 'execute_result')
-                self.assertIn('text/plain', output_msgs[0].content['data'])
-                self.assertEqual(output_msgs[0].content['data']['text/plain'],
+                self.assertEqual(output_msgs[0]['msg_type'], 'execute_result')
+                self.assertIn('text/plain', output_msgs[0]['content']['data'])
+                self.assertEqual(output_msgs[0]['content']['data']['text/plain'],
                                  sample['result'])
 
     code_display_data = []
@@ -236,26 +226,30 @@ class KernelTests(TestCase):
 
         for sample in self.code_display_data:
             with self.subTest(code=sample['code']):
+                self.flush_channels()
                 reply, output_msgs = self.execute_helper(sample['code'])
 
-                self.assertEqual(reply.content['status'], 'ok')
+                self.assertEqual(reply['content']['status'], 'ok')
 
                 self.assertGreaterEqual(len(output_msgs), 1)
-                self.assertEqual(output_msgs[0].header['msg_type'], 'display_data')
-                self.assertIn(sample['mime'], output_msgs[0].content['data'])
+                self.assertEqual(output_msgs[0]['msg_type'], 'display_data')
+                self.assertIn(sample['mime'], output_msgs[0]['content']['data'])
 
     # this should match one of the values in code_execute_result
     code_history_pattern = ""
     supported_history_operations = ()
 
     def history_helper(self, execute_first, timeout=TIMEOUT, **histargs):
+        self.flush_channels()
+
         for code in execute_first:
             reply, output_msgs = self.execute_helper(code)
 
-        fut = self.kc.history(**histargs, reply=False)
+        self.flush_channels()
+        msg_id = self.kc.history(**histargs)
 
-        reply = self._wait_for_reply(fut, timeout=timeout)
-        validate_message(reply, 'history_reply', fut.jupyter_msg_id)
+        reply = self.kc.get_shell_msg(timeout=timeout)
+        validate_message(reply, 'history_reply', msg_id)
 
         return reply
 
@@ -274,15 +268,15 @@ class KernelTests(TestCase):
                 raise SkipTest
             reply = self.history_helper(codes, output=False, raw=True,
                                         hist_access_type="tail", n=n)
-            self.assertEqual(len(reply.content['history']), n)
-            self.assertEqual(len(reply.content['history'][0]), 3)
-            self.assertEqual(codes, [h[2] for h in reply.content['history']])
+            self.assertEqual(len(reply['content']['history']), n)
+            self.assertEqual(len(reply['content']['history'][0]), 3)
+            self.assertEqual(codes, [h[2] for h in reply['content']['history']])
 
-            session, start = reply.content['history'][0][0:2]
+            session, start = reply['content']['history'][0][0:2]
             with self.subTest(output=True):
                 reply = self.history_helper(codes, output=True, raw=True,
                                             hist_access_type="tail", n=n)
-                self.assertEqual(len(reply.content['history'][0][2]), 2)
+                self.assertEqual(len(reply['content']['history'][0][2]), 2)
 
         with self.subTest(hist_access_type="range"):
             if 'range' not in self.supported_history_operations:
@@ -293,9 +287,9 @@ class KernelTests(TestCase):
                                         hist_access_type="range",
                                         session=session, start=start,
                                         stop=start+1)
-            self.assertEqual(len(reply.content['history']), 1)
-            self.assertEqual(reply.content['history'][0][0], session)
-            self.assertEqual(reply.content['history'][0][1], start)
+            self.assertEqual(len(reply['content']['history']), 1)
+            self.assertEqual(reply['content']['history'][0][0], session)
+            self.assertEqual(reply['content']['history'][0][1], start)
 
         with self.subTest(hist_access_type="search"):
             if not self.code_history_pattern:
@@ -307,23 +301,19 @@ class KernelTests(TestCase):
                 reply = self.history_helper(codes, output=False, raw=True,
                                             hist_access_type="search",
                                             pattern=self.code_history_pattern)
-                self.assertGreaterEqual(len(reply.content['history']), 1)
+                self.assertGreaterEqual(len(reply['content']['history']), 1)
             with self.subTest(subsearch="unique"):
                 reply = self.history_helper(codes, output=False, raw=True,
                                             hist_access_type="search",
                                             pattern=self.code_history_pattern,
                                             unique=True)
-                entries = [x[2] for x in reply.content['history']]
-                if len(entries) > len(set(entries)):
-                    raise AssertionError("History search results not unique: {}"
-                                         .format(sorted(entries)))
-
+                self.assertEqual(len(reply['content']['history']), 1)
             with self.subTest(subsearch="n"):
                 reply = self.history_helper(codes, output=False, raw=True,
                                             hist_access_type="search",
                                             pattern=self.code_history_pattern,
                                             n=3)
-                self.assertEqual(len(reply.content['history']), 3)
+                self.assertEqual(len(reply['content']['history']), 3)
 
     code_inspect_sample = ""
 
@@ -331,13 +321,14 @@ class KernelTests(TestCase):
         if not self.code_inspect_sample:
             raise SkipTest
 
-        fut = self.kc.inspect(self.code_inspect_sample, reply=False)
-        reply = self._wait_for_reply(fut)
-        validate_message(reply, 'inspect_reply', fut.jupyter_msg_id)
+        self.flush_channels()
+        msg_id = self.kc.inspect(self.code_inspect_sample)
+        reply = self.kc.get_shell_msg(timeout=TIMEOUT)
+        validate_message(reply, 'inspect_reply', msg_id)
 
-        self.assertEqual(reply.content['status'], 'ok')
-        self.assertTrue(reply.content['found'])
-        self.assertGreaterEqual(len(reply.content['data']), 1)
+        self.assertEqual(reply['content']['status'], 'ok')
+        self.assertTrue(reply['content']['found'])
+        self.assertGreaterEqual(len(reply['content']['data']), 1)
 
     code_clear_output = ""
 
@@ -345,9 +336,10 @@ class KernelTests(TestCase):
         if not self.code_clear_output:
             raise SkipTest
 
+        self.flush_channels()
         reply, output_msgs = self.execute_helper(code=self.code_clear_output)
 
-        self.assertEqual(reply.content['status'], 'ok')
+        self.assertEqual(reply['content']['status'], 'ok')
 
         self.assertGreaterEqual(len(output_msgs), 1)
-        self.assertEqual(output_msgs[0].header['msg_type'], 'clear_output')
+        self.assertEqual(output_msgs[0]['msg_type'], 'clear_output')
