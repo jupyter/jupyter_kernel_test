117fcac5a6af756d967832708054bce03602b7ec
Steven Silvester <steven.silvester@ieee.org>
2021-12-06 15:10:28 -0600

    fix windows test

--- test_irkernel.py
+++ test_irkernel.py
@@ -4,6 +4,7 @@ A non-python example, with tests for IRKernel (irkernel.github.io).
 language being tested)
 """
 
+import os
 import unittest
 
 from jupyter_client.kernelspec import KernelSpecManager, NoSuchKernel
@@ -30,7 +31,7 @@ class IRKernelTests(jkt.KernelTests):
             'text': 'zi',
             'matches': {'zip'},
         },
-    ]
+    ] if os.name != 'nt' else []  # zip is not available on Windows
 
     complete_code_samples = ['1', "print('hello, world')", "f <- function(x) {x*2}"]
     incomplete_code_samples = ["print('hello", "f <- function(x) {x"]
@@ -43,10 +44,5 @@ class IRKernelTests(jkt.KernelTests):
     ]
 
 
-
 if __name__ == '__main__':
-    import os
-    # zip is not available on Windows
-    if os.name == 'nt':
-        IRKernelTests.completion_samples = []
     unittest.main()
