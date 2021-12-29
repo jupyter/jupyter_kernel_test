fd5889c868a35baf3521fb6a36f216e72a9a1491
Steven Silvester <steven.silvester@ieee.org>
2021-12-29 16:15:25 -0600

    try this

--- test_julia.py
+++ test_julia.py
@@ -2,6 +2,7 @@ from jupyter_client import KernelManager
 import logging
 
 km = KernelManager(kernel_name="julia-1.7")
+logging.basicConfig()
 km.log.setLevel(logging.DEBUG)
 km.start_kernel()
 kc = km.client()
@@ -9,6 +10,7 @@ kc.log.setLevel(logging.DEBUG)
 kc.start_channels()
 try:
     kc.wait_for_ready(timeout=60)
-except RuntimeError:
+finally:
     kc.stop_channels()
     km.shutdown_kernel()
+
