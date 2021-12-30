cd7c0a8f55f50e41b763f86d55c236ebaf6c35ad
Steven Silvester <steven.silvester@ieee.org>
2021-12-29 21:24:34 -0600

    .

---
+++ test_xues_cling.py
@@ -0,0 +1,43 @@
+"""
+A non-python example, with tests for IRKernel (irkernel.github.io).
+(Beware of python quoting/string escaping rules being different to the
+language being tested)
+"""
+
+import unittest
+import shutil
+
+from jupyter_client.kernelspec import NoSuchKernel
+import jupyter_kernel_test as jkt
+
+
+class XeusClingKernelTests(jkt.KernelTests):
+    kernel_name = "C++17"
+
+    @classmethod
+    def setUpClass(cls):
+        try:
+            cls.km, cls.kc = jkt.start_new_kernel(kernel_name=cls.kernel_name)
+        except NoSuchKernel:
+            raise unittest.SkipTest('Xeus-Cling Kernel not installed')
+
+    language_name = "C++17"
+
+    file_extension = ".cpp"
+
+    code_hello_world = '#include <iostream>\nstd::cout << "hello, world!" << std::endl;'
+
+    code_stderr = '#include <iostream>\nstd::cerr << "some error" << std::endl;'
+
+    complete_code_samples = ['1', "int j=5"]
+    incomplete_code_samples = ["double sqr(double a"]
+
+    code_generate_error = 'throw std::runtime_error("Unknown exception");'
+
+    code_execute_result = [
+        {'code': 'int j = 5;j', 'result': "5"},
+    ]
+
+
+if __name__ == '__main__':
+    unittest.main()
